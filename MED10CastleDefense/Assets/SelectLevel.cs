973b379be0aa2aa7da5778729cfaa752a119c180
LasseR <lasse_1302@hotmail.com>
2017-03-31 11:21:41 +0200

    Created a statemanager Created an eventmanager

---
+++ MED10CastleDefense/Assets/SelectLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectLevel : MonoBehaviour {
+
+    public string LevelName;
+    public int LevelNumber;
+
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>UpdateState());
+
+    }
+    void UpdateState()
+    {
+        var instance = StateManager.Instance;
+        instance.LevelName = LevelName;
+        instance.SelectedLevel = LevelNumber;
+    }
+}
