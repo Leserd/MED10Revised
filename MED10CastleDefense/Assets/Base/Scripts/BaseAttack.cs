e64027bd1a99fefffe146bf60888242e23e27c0f
Andreas Bork <Ahbork@gmail.com>
2017-03-31 14:48:10 +0200

    no message

---
+++ MED10CastleDefense/Assets/Base/Scripts/BaseAttack.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAttack : MonoBehaviour {
+
+    public int damage = 2;
+    public float attackSpeed = 1f;
+    public float splashRadius = 1f;
+    public float attackRange;
+    public GameObject projectilePrefab;
+    public E_AttackType attackType;
+    public bool canAttack = false;  //
+    private List<GameObject> _availableTargets = new List<GameObject>();
+    private GameObject _target;
+    private Coroutine _attackCoroutine;
+
+    private void OnEnable()
+    {
+        //EventManager.instance.StartListening("SpawnUnit", AddTarget);
+    }
+
+
+
+    public void AddTarget(GameObject newTarget)
+    {
+        _availableTargets.Add(newTarget);
+    }
+
+
+
+    public void StartAttacking()
+    {
+        if(_attackCoroutine == null)
+        {
+            _attackCoroutine = StartCoroutine(Attack());
+        }
+    }
+
+
+
+    private IEnumerator Attack()
+    {
+        while (canAttack && _availableTargets.Count > 0)
+        {
+            _target = GetClosestTarget();
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            //projectile.GetComponent<Projectile>().owner = this;
+            yield return new WaitForSeconds(attackSpeed);
+        }
+    }
+
+
+
+    private GameObject GetClosestTarget()
+    {
+        GameObject closestTarget = GameObject.FindGameObjectWithTag("PlayerBase");
+
+        foreach(GameObject go in _availableTargets)
+        {
+            if(Vector2.Distance(go.transform.position, transform.position) < Vector2.Distance(closestTarget.transform.position, transform.position))
+            {
+                closestTarget = go;
+            }
+        }
+
+        return closestTarget;
+    }
+
+}
+
+
+
+public enum E_AttackType
+{
+    SINGLE,
+    SPLASH
+}
---
+++ MED10CastleDefense/Assets/Base/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private float _damage;
+    private float _splashRadius;
+    private E_AttackType _type;
+
+
+
+    public void SetOwner(BaseAttack owner)
+    {
+        _damage = owner.damage;
+        _type = owner.attackType;
+        _splashRadius = owner.splashRadius;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Unit")
+        {
+            List<GameObject> targets = new List<GameObject>();
+            if(_type == E_AttackType.SINGLE)
+            {
+                targets.Add(collision.gameObject);
+            }
+            else if(_type == E_AttackType.SPLASH)
+            {
+                Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, _splashRadius);
+                foreach(Collider2D hit in hits)
+                {
+                    targets.Add(hit.gameObject);
+                }
+            }
+
+            foreach(GameObject target in targets)
+            {
+                //TODO: Send damager dealer and damage taker when triggering event
+                //EventManager.TriggerEvent("DealDamage");
+                print(transform.name + " hit: " + target.name + " for " + _damage + " damage.");
+            }
+        }
+    }
+
+}
