f982adb816651f79b3a5bfa1991fa4a08e8faa5e
Andreas Bork <Ahbork@gmail.com>
2017-04-17 10:11:04 +0200

    Nye hints smidt ind, Graph overview created

---
+++ MED10CastleDefense/Assets/GraphOverview/GraphOverview.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphOverview : MonoBehaviour {
+    public Button next, last;
+    public Image graphImg;
+    public Sprite[] graphs;
+    private int _curIndex = 0;
+
+
+    private void Awake()
+    {
+        if (graphImg == null)
+            graphImg = transform.GetChild(0).GetComponent<Image>();
+        if(next == null)
+            next = transform.GetChild(1).GetComponent<Button>();
+        if (last == null)
+            last = transform.GetChild(2).GetComponent<Button>();
+
+        next.onClick.AddListener(() => ShowNewImage(1));
+        last.onClick.AddListener(() => ShowNewImage(-1));
+
+        if (graphs.Length == 0)
+            this.enabled = false;
+        else
+            graphImg.sprite = graphs[0];
+
+        last.interactable = false;
+    }
+
+
+    public void ShowNewImage(int direction)
+    {
+        _curIndex += direction;
+        if (_curIndex <= 0)
+        {
+            _curIndex = 0;
+            last.interactable = false;
+            next.interactable = true;
+            
+        }
+        else if (_curIndex >= graphs.Length - 1)
+        {
+            _curIndex = graphs.Length - 1;
+            last.interactable = true;
+            next.interactable = false;
+        }
+        else
+        {
+            last.interactable = true;
+            next.interactable = true;
+        }
+
+        graphImg.sprite = graphs[_curIndex];
+    }
+	
+}
