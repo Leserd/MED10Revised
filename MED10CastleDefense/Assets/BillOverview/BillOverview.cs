f3be35df2faca3e5d458e48931768a8b26794608
Andreas Bork <Ahbork@gmail.com>
2017-04-19 10:23:23 +0200

    BillOverview ready for month-sprites

--- MED10CastleDefense/Assets/BillOverview/BillOverview.cs
+++ MED10CastleDefense/Assets/BillOverview/BillOverview.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class BillOverview : MonoBehaviour {
 
-    public Text[] panelTexts;
-
+    Text[] panelTexts;
+    Transform monthsParent;
+    public Sprite[] monthsToPay;
+    public Sprite[] monthsToNotPay;
     
     void Awake()
     {
         panelTexts = GetComponentsInChildren<Text>();
+        monthsParent = transform.GetChild(4);
         EventManager.StartListening("SelectedLevel", SelectedLevel);
     }
 
@@ -27,14 +30,18 @@ public class BillOverview : MonoBehaviour {
         panelTexts[2].text = PretendData.Instance.Data[lvl].BSDataFrequency;
         panelTexts[3].text = PretendData.Instance.Data[lvl].BSDataAmount + " kr";
 
-        int startIndex = 4;
-        for (int i = 0; i < panelTexts.Length; i++)
+        //Show payment months
+        if (monthsToPay.Length != 0 && monthsToNotPay.Length != 0)
         {
-            Color textColor = new Color();
-            if (PretendData.Instance.Data[i].BSDataPaymentMonths.Contains(i)){
-                //textColor = 
+            for (int i = 0; i < monthsParent.childCount; i++)
+            {
+                Sprite paymentSprite = monthsToNotPay[i];
+                if (PretendData.Instance.Data[i].BSDataPaymentMonths.Contains(i))
+                {
+                    paymentSprite = monthsToPay[i];
+                }
+                monthsParent.GetChild(i).GetComponent<Image>().sprite = paymentSprite;
             }
-           // panelTexts[startIndex + i].color = 
         }
     }
 
