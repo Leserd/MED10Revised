778e96d70e3dff556cb0d8403e2cb36c2146a4e2
Andreas Bork <Ahbork@gmail.com>
2017-04-20 16:14:56 +0200

    TableOverview er godt på vej

---
+++ MED10CastleDefense/Assets/GraphOverview/TableOverview.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TableOverview : MonoBehaviour {
+
+    public GameObject rowPrefab;
+    private GameObject _topRow, _botRow;
+
+    private List<TableOverviewRow> bills = new List<TableOverviewRow>();
+    int testNum = 0;
+
+    private void Awake()
+    {
+        _topRow = transform.GetChild(0).gameObject;
+        _botRow = transform.GetChild(1).gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            TableOverviewRow newBill = Instantiate(rowPrefab, transform).GetComponent<TableOverviewRow>();
+            newBill.Fill(PretendData.Instance.Data[testNum]);
+            bills.Add(newBill);
+            testNum++;
+            _botRow.transform.SetAsLastSibling();
+            UpdateTotal();
+        }
+    }
+
+    private void UpdateTotal()
+    {
+        Text[] botRowTexts = _botRow.transform.GetComponentsInChildren<Text>();
+
+        botRowTexts[0].text = "Total";
+        for (int i = 0; i < 12; i++)
+        {
+            int totalMonth = 0;
+            for (int k = 0; k < bills.Count; k++)
+            {
+                int monthNum = 0;
+                if (bills[k].GetRowText(i + 1).text != string.Empty)
+                {
+                    monthNum = int.Parse(bills[k].GetRowText(i + 1).text);
+                }
+                totalMonth += monthNum;
+            }
+            botRowTexts[i + 1].text = totalMonth.ToString();
+        }
+    }
+}
---
+++ MED10CastleDefense/Assets/GraphOverview/TableOverviewRow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TableOverviewRow : MonoBehaviour {
+
+    private Text[] _rowTexts;   //0 = name, 1-12 = months, 13 = avg, 14 = total
+    private Image _background;
+    private Color _colorWhite = new Color(1, 1, 1, 0.7f);
+    private Color _colorGrey = new Color(0.9f, 0.9f, 0.9f, 0.7f);
+
+    private void Awake()
+    {
+
+        _rowTexts = transform.GetComponentsInChildren<Text>();
+        _background = GetComponent<Image>();
+        _background.color = transform.GetSiblingIndex() % 2 > 0 ? _colorWhite : _colorGrey;
+    }
+
+
+    public void Fill(InputData bill)
+    {
+        _rowTexts[0].text = bill.BSDataName;
+        for (int i = 0; i < bill.BSDataPaymentMonths.Count; i++)
+        {
+            _rowTexts[bill.BSDataPaymentMonths[i] + 1].text = bill.BSDataAmountMonthly;
+        }
+        _rowTexts[13].text = Mathf.RoundToInt(int.Parse(bill.BSDataAmount) / 12).ToString();
+        _rowTexts[14].text = Mathf.RoundToInt(int.Parse(bill.BSDataAmount)).ToString();
+
+    }
+
+    public Text GetRowText(int index)
+    {
+        return _rowTexts[index];
+    }
+}
