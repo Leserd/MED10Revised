3c9680eea14c99b95637bd1df05ab82074f862fe
LasseR <lasse_1302@hotmail.com>
2017-04-25 13:40:49 +0200

    Created a budget information button

---
+++ MED10CastleDefense/Assets/BudgetInformation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BudgetInformation : MonoBehaviour {
+    public GameObject InfoText;
+
+	// Use this for initialization
+	void Awake () {
+        GetComponentInChildren<Button>().onClick.AddListener(() => InfoText.SetActive(!InfoText.activeSelf));
+	}
+	
+
+}
